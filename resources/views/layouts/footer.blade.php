c45fe13f7ce42ca8794b7c163ea2f280d1282792
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2023-11-20 18:07:53 +0700

    update template

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
     <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2023 Kelompok 2.</strong>
+    <strong>Copyright &copy; 2023 Mahardhika Bredy Dwi G.S.</strong>
 </footer>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{url('/')}}" class="brand-link">
-        <img src="{{asset('assets/logo.jpg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-bold">Kios Sahabat Tani</span>
+        <span class="brand-text font-weight-bold">SPK EDAS</span>
     </a>
 
     <!-- Sidebar -->
@@ -17,7 +16,7 @@
                 {{--          <a href="#" class="d-block">{{Auth::user()->username}}</a>--}}
                 <a href="{{url('profile')}}" class="d-block">
                     <i class="fas fa-user mx-1"></i>
-                    User
+                    Untitled
                 </a>
             </div>
         </div>
